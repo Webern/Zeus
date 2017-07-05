481cb206edad2da5ecf96dbda998871ef07aebce
Matthew James Briggs <matthew.james.briggs@gmail.com>
2017-07-04 23:07:23 -0700

    enums and crossguid

---
+++ Sourcecode/include/zeus/Guid.h
@@ -0,0 +1,27 @@
+// Lyre, Copyright 2017 by Matthew James Briggs
+
+#pragma once
+
+#include <cstdint>
+
+namespace lyre
+{
+	/// @brief TODO
+	///
+	/// @detailed TODO
+	///
+    class Guid
+    {
+    public:
+    	Guid();
+
+    public:
+    	void pubicFunc() const;
+
+    private:
+    	int myInt;
+
+    private:
+        void privateFunc();     
+    };
+}
